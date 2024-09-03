eae086891abf722df67ca1497dbf3475a55d3535
Ethan Ferguson <ethanf108@gmail.com>
2024-09-03 13:48:32 -0400

    Added a warning when update fails

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-git pull --ff-only 1>/dev/null 2>/dev/null &
+git pull --ff-only 1>/dev/null 2>/dev/null || touch $HOME/.scripts-update-failed &
